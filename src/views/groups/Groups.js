ae14886f9faafc36616920deb3460032259ceff7
NaumanMoazzam <naumanmoazzam3@gmail.com>
2021-12-09 23:08:45 +0500

    removing unnecassry line

--- src/views/groups/Groups.js
+++ src/views/groups/Groups.js
@@ -503,9 +503,9 @@ const Groups = () => {
                       )}
                       </tbody>
                       {checkForTimer && <ul class="pagination">
-                      <li class="page-item"><a class="page-link" href="#">1</a></li>
-                      <li class="page-item"><a class="page-link" href="#">2</a></li>
-                      <li class="page-item"><a class="page-link" href="#">3</a></li>
+                      <li class="page-item"><a class="page-link">1</a></li>
+                      <li class="page-item"><a class="page-link">2</a></li>
+                      <li class="page-item"><a class="page-link">3</a></li>
                     </ul>}
                     </table>
                   )}
