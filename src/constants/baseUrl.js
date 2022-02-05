2953c29eaec58ddfd3e5e358c29d48d866303dbe
nabilshafi <nabil.shafi1@gmail.com>
2022-02-05 16:03:18 +0500

    fix the url link

--- src/constants/baseUrl.js
+++ src/constants/baseUrl.js
@@ -1,4 +1,4 @@
-export const baseUrl =
-  "http://ec2-18-116-115-34.us-east-2.compute.amazonaws.com:7080/api/v1";
-export const testBaseUrl =
-  "http://ec2-18-116-115-34.us-east-2.compute.amazonaws.com:7080/api/v1";
+export const baseUrl ="http://ec2-18-118-168-165.us-east-2.compute.amazonaws.com:7080/api/v1";
+ // "http://ec2-18-116-115-34.us-east-2.compute.amazonaws.com:7080/api/v1";
+export const testBaseUrl ="http://ec2-18-118-168-165.us-east-2.compute.amazonaws.com:7080/api/v1";
+ // "http://ec2-18-116-115-34.us-east-2.compute.amazonaws.com:7080/api/v1";
